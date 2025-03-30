9e7b183bb5269521320d1bd622bfd2722165844f
Owusu Bright Omari <owusuomaribright@gmail.com>
2025-03-30 00:46:49 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -243,18 +243,6 @@ const ProjectLink = styled.a`
 
 const Projects = () => {
   const [comets, setComets] = useState([]);
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-    
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-    
-    return () => window.removeEventListener('resize', checkMobile);
-  }, []);
 
   useEffect(() => {
     const generateComet = () => {
